246a39bb90a61ef812f57e3957649408d37a83af
HisameOgasahara <hisamexinori@gmail.com>
2026-06-02 16:34:04 +0900

    Add files via upload

--- 26_06_02/publisher.py
+++ 26_06_02/publisher.py
@@ -1,88 +1,147 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
 import cv2
+import json
 import rclpy
+import numpy as np
+
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import String, Bool
 
-def gstreamer_pipeline(
-    sensor_id=0,
-    capture_width=1280,
-    capture_height=720,
-    display_width=1280,
-    display_height=720,
-    framerate=30,
-    flip_method=2, #상하좌우 반전. 2는 180도 회전, 1은 90도 회전    
-):
-    return (
-        "nvarguscamerasrc sensor-id=%d ! " 
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            sensor_id,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-    
+from jetson_inference import detectNet
+from jetson_utils import videoSource
 
-class CSIVideoPublisher(Node):
+
+class DetectNetNode(Node):
     def __init__(self):
-        super().__init__('csi_video_publisher')
-        
-        self.publisher = self.create_publisher(
+        super().__init__('detectnet_node')
+
+        #---------------------------------
+        # publishers
+        #---------------------------------
+        self.img_pub = self.create_publisher(
             CompressedImage, 'camera', 5
         )
-
-        pipeline = gstreamer_pipeline()
-        self.get_logger().info(f"Gstreamer pipeline:\n{pipeline}")
-
-        self.cap = cv2.VideoCapture(
-            pipeline, cv2.CAP_GSTREAMER
+        self.result_pub = self.create_publisher(
+            String, '/detectnet/result', 5
         )
 
-        if not self.cap.isOpened():
-            self.get_logger().error("Failed to CSI camera with Gstreamer")
-            raise RuntimeError("CSI camera open failed")
-        
-        self.timer = self.create_timer(1.0/30.0, self.timer_callback)
-        self.get_logger().info("CSI camera video publisher started")
-
-    def timer_callback(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            self.get_logger().warn("Frame capture failed")
-            return
-        success, jpg = cv2.imencode(
-            ".jpg", frame, [cv2.IMWRITE_JPEG_QUALITY, 80]
+        #---------------------------------
+        # Subscriber
+        #---------------------------------
+        self.inference_enabled = False
+        self.create_subscription(
+            Bool, 
+            '/inference_switch',
+            self.switch_callback,
+            5
         )
-        if not success:
-            return
-        
-        msg = CompressedImage()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.format = "jpeg"
-        msg.data = jpg.tobytes()
 
-        self.publisher.publish(msg)
+        self.get_logger().info("DetectNet node started")
+
+
+    def switch_callback(self, msg):
+        self.inference_enabled = msg.data
+        self.get_logger().info(
+            f"Inference switch: {'ON' if msg.data else 'OFF'}"
+        )
+
 
 def main():
     rclpy.init()
-    node = CSIVideoPublisher()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
+    node = DetectNetNode()
+
+    #---------------------------------
+    # Jetson Inference Init
+    #---------------------------------
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(base_dir, 'ssd-mobilenet.onnx')
+    labels_path = os.path.join(base_dir, 'labels.txt')
+
+    input_uri = 'csi://0'  # CSI 카메라 입력 URI
+    overlay = "none"
+    threshold = 0.5
+
+    input_stream = videoSource(input_uri)
+
+    net = detectNet(
+        argv=[
+            "--model=" + model_path,
+            "--labels=" + labels_path,
+            "--input-blob=input_0",
+            "--output-cvg=scores",
+            "--output-bbox=boxes",
+            "--threshold=" + str(threshold),
+        ]
+    )
+
+    #-------------------------------
+    # Main Loop
+    #-------------------------------
+    while rclpy.ok():
+        img_cuda = input_stream.Capture()
+        if img_cuda is None:
+            continue
+        detections = net.Detect(img_cuda, overlay=overlay)
+
+        # ===============================
+        # 1) Publish image
+        # ===============================
+        img_np = np.array(img_cuda, copy=True)
+        img_bgr =cv2.cvtColor(img_np, cv2.COLOR_RGBA2BGR)
+
+        # ===============================
+        # 2) Inference
+        # ===============================
+        if node.inference_enabled:
+            result = []
+            for d in detections:
+                xmin = int(d.Left)
+                ymin = int(d.Top)
+                xmax = int(d.Right)
+                ymax = int(d.Bottom)
+                class_name = net.GetClassDesc(d.ClassID)
+                conf = float(d.Confidence)
+                label = f"{class_name}: {conf:.2f}"
+
+                cv2.rectangle(img_bgr, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                
+                cv2.putText(img_bgr, label, (xmin, max(ymin - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                
+                result.append({"class": class_name, "confidence": conf,
+                    "xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax,})
+
+            result_msg = String()
+            result_msg.data = json.dumps(result)
+            node.result_pub.publish(result_msg)
+
+        _, jpg = cv2.imencode('.jpg', img_bgr, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        img_msg = CompressedImage()
+        img_msg.header.stamp = node.get_clock().now().to_msg()
+        img_msg.format = 'jpeg'
+        img_msg.data = jpg.tobytes()
+        node.img_pub.publish(img_msg)
+
+        # ROS spin
+        rclpy.spin_once(node, timeout_sec=0.0)
+        if not input_stream.IsStreaming():
+            break
     
-    node.cap.release()
     node.destroy_node()
     rclpy.shutdown()
 
